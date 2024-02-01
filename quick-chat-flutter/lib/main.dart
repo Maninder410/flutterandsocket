d92f8b58f9f4827fed178b3ab3375b2a90aabf33
maninder <manindersingh29650@gmail.com>
2024-02-01 15:59:52 +0530

    try

--- quick-chat-flutter/lib/main.dart
+++ quick-chat-flutter/lib/main.dart
@@ -13,7 +13,7 @@ class QuickChatApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: appName,
+        title: appName, 
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
